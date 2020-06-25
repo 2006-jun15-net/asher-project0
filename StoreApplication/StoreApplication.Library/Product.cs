2e018c27c062062193a46e04c339f15ef48ce525
Asher Williams <asher.williams1998@gmail.com>
2020-06-24 21:01:13 -0500

    added properties, fields, and constructor for Product

--- StoreApplication/StoreApplication.Library/Product.cs
+++ StoreApplication/StoreApplication.Library/Product.cs
@@ -6,5 +6,44 @@ namespace StoreApplication.Library
 {
     public class Product
     {
+        private static int idSeed = 1;
+
+        private string _name;
+        private double _price;
+        private int _maxPurchaseAmount;
+        private int _productID;
+
+        public string name
+        {
+            get => _name;
+
+            set
+            {
+
+            }
+        }
+        public double price
+        {
+            get => _price;
+
+            set
+            {
+
+            }
+        }
+        public int maxPurchaseAmount
+        {
+            get => _maxPurchaseAmount;
+        }
+
+        public Product(string name, double price, int maxPurchaseAmount)
+        {
+            this._productID = idSeed;
+            idSeed++;
+
+            this._name = name;
+            this._price = price;
+            this._maxPurchaseAmount = maxPurchaseAmount;
+        }
     }
 }
