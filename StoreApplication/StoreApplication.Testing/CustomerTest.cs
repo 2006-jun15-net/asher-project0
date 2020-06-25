770c5b117625b4fcb7af8b205dafddf13da56df9
Asher Williams <asher.williams1998@gmail.com>
2020-06-24 21:28:00 -0500

    created CustomerTest file

---
+++ StoreApplication/StoreApplication.Testing/CustomerTest.cs
@@ -0,0 +1,16 @@
+using StoreApplication.Library;
+using System;
+using Xunit;
+
+namespace StoreApplication.Testing
+{
+    public class CustomerTest
+    {
+        readonly Customer customer = new Customer();
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
