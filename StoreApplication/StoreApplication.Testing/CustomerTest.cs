fb5925189274e83c43571b8f43fc88460dfddcd6
Asher Williams <asher.williams1998@gmail.com>
2020-06-24 22:14:39 -0500

    added validation to the setting of setting customer first name to ensure that it does allow null values and added a test for it in the test project

--- StoreApplication/StoreApplication.Testing/CustomerTest.cs
+++ StoreApplication/StoreApplication.Testing/CustomerTest.cs
@@ -7,10 +7,17 @@ namespace StoreApplication.Testing
     public class CustomerTest
     {
         readonly Customer customer = new Customer();
+
         [Fact]
         public void FirstName_EmptyValue_ThrowsException()
         {
             Assert.ThrowsAny<ArgumentException>(() => customer.firstName = string.Empty);
+        }
+
+        [Fact]
+        public void FirstName_NullValue_ThrowsException()
+        {
+            Assert.ThrowsAny<ArgumentException>(() => customer.firstName = null);
         }
     }
 }
