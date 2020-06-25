a31601409cbefb413822a21fc8140ad0d94b0a98
Asher Williams <asher.williams1998@gmail.com>
2020-06-25 11:55:58 -0500

    Created ExistingOrNew method with input validation

--- StoreApplication/StoreApplication.ConsoleApp/Program.cs
+++ StoreApplication/StoreApplication.ConsoleApp/Program.cs
@@ -14,27 +14,7 @@ namespace StoreApplication.ConsoleApp
         {
             //initializeData();
             Store.LoadData();
-            string input;
-            while(true)
-            {
-                input = UI.IntroMenu();
-                if(input == "1")
-                {
-                    Console.WriteLine();
-                    Console.WriteLine("Signing In");
-                    break;
-                }
-                else if(input == "2")
-                {
-                    break;
-                }
-                else
-                {
-                    Console.WriteLine();
-                    Console.WriteLine("That's not a valid choice. Please try again.");
-                    Console.WriteLine();
-                }
-            }
+            UI.IntroMenu();
             
             //Console.WriteLine($"Location Count: {Store.LocationData.Count}");
             //Console.WriteLine($"Product Count: {Store.ProductData.Count}");
--- StoreApplication/StoreApplication.ConsoleApp/UI.cs
+++ StoreApplication/StoreApplication.ConsoleApp/UI.cs
@@ -6,15 +6,74 @@ namespace StoreApplication.ConsoleApp
 {
     public static class UI
     {
-        public static string IntroMenu()
+        private static string input;
+        public static void IntroMenu()
         {
-            Console.WriteLine("Welcome to the store!");
-            Console.WriteLine("What would you like to do?");
-            Console.WriteLine("1.  Sign in");
-            Console.WriteLine("2.  Exit store");
-            Console.Write("Please enter a valid choice: ");
-            var input = Console.ReadLine();
-            return input;
+            while(true)
+            {
+                Console.WriteLine("Welcome to the store!");
+                Console.WriteLine("What would you like to do?");
+                Console.WriteLine("1.  Sign in");
+                Console.WriteLine("2.  Exit store");
+                Console.Write("Please enter a valid choice: ");
+                input = Console.ReadLine();
+                if(input == "1")
+                {
+                    ExistingOrNew();
+                }
+                else if(input == "2")
+                {
+                    break;
+                }
+                else
+                {
+                    InvalidInput();
+                }
+            }
+        }
+
+        public static void ExistingOrNew()
+        {
+            Console.WriteLine();
+            while (true)
+            {
+                Console.WriteLine("Are you an existing customer or a new one?");
+                Console.WriteLine("1.  New");
+                Console.WriteLine("2.  Existing");
+                Console.WriteLine("3.  Go back to previous menu");
+                Console.Write("Please enter a valid choice: ");
+                input = Console.ReadLine();
+
+                if(input == "1")
+                {
+                    Console.WriteLine("New Customer");
+                    Console.WriteLine();
+                    break;
+                }
+                else if(input == "2")
+                {
+                    Console.WriteLine("Existing Customer");
+                    Console.WriteLine();
+                    break;
+                }
+                else if(input == "3")
+                {
+                    Console.WriteLine();
+                    break;
+                }
+                else
+                {
+                    InvalidInput();
+                }
+            }
+            
+        }
+
+        public static void InvalidInput()
+        {
+            Console.WriteLine();
+            Console.WriteLine("That's not a valid choice. Please try again.");
+            Console.WriteLine();
         }
     }
 }
