aa3d9b7bc3e9964c7fb77252a47509477a243de0
Asher Williams <asher.williams1998@gmail.com>
2020-06-25 10:58:54 -0500

    created Store class and its properties and fields

---
+++ StoreApplication/StoreApplication.Library/Store.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StoreApplication.Library
+{
+    public class Store
+    {
+        private List<Product> _productData;
+        private List<Location> _locationData;
+        private List<Customer> _customerData;
+
+        public List<Product> ProductData
+        {
+            get => _productData;
+        }
+        public List<Location> LocationData
+        {
+            get => _locationData;
+        }
+        public List<Customer> CustomerData
+        {
+            get => _customerData;
+        }
+
+        public Store()
+        {
+
+        }
+    }
+}
