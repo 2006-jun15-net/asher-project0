ba51515fcb1290c812caf738e88957ac12ca3d45
Asher Williams <asher.williams1998@gmail.com>
2020-06-24 20:44:26 -0500

    added properties, fields, and constructor to Location

--- StoreApplication/StoreApplication.Library/Location.cs
+++ StoreApplication/StoreApplication.Library/Location.cs
@@ -6,5 +6,30 @@ namespace StoreApplication.Library
 {
     class Location
     {
+        private static int idSeed = 1;
+
+        private string _address;
+        private List<Order> _orderHistory = new List<Order>();
+        private Dictionary<Product, int> _inventory = new Dictionary<Product, int>();
+        private int _locationID;
+
+        public string address
+        {
+            get => _address;
+            set
+            {
+                _address = value;
+            }
+        }
+        public List<Order> OrderHistory { get; set; } = new List<Order>();
+        public Dictionary<Product, int> Inventory { get; set; } = new Dictionary<Product, int>();
+
+        public Location(string address)
+        {
+            this._locationID = idSeed;
+            idSeed++;
+
+            this._address = address;
+        }
     }
 }
